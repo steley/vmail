8426ebd2bb1acb5b9ed8fbdbe05f56b69b6af235
S <4951789+steley@users.noreply.github.com>
2026-01-30 14:46:06 +0800

    Update Footer component with new copyright and links

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,5 @@
 import { Link } from "react-router-dom";
-import Twitter from "./icons/Twitter.tsx";
-import GitHub from "./icons/GitHub.tsx";
 import MailIcon from "./icons/MailIcon.tsx";
-import WrdoLogo from "./icons/Wrdo.tsx";
 
 /**
  * 网站的页脚组件
@@ -10,52 +7,16 @@ import WrdoLogo from "./icons/Wrdo.tsx";
 export function Footer() {
   return (
     <div className="text-white w-full mt-auto flex flex-col items-center justify-between px-5 pt-16 mb-10 md:px-10 mx-auto sm:flex-row">
-      <Link to="/" className="text-xl font-black leading-none select-none logo">
-        VMAIL.DEV
-      </Link>{" "}
       <p className="mt-4 text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">
-        © 2024 Products of{" "}
-        <a
-          className="font-semibold underline hover:text-gray-600"
-          href="https://www.oiov.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          oiov
-        </a>
-        .
+        © 2026 TempMail.cc
       </p>
       <div className="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
         {/* 社交媒体和联系方式链接 */}
         <a
-          href="https://like.do"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="LikeDo"
-          className="text-gray-400 hover:text-gray-500  scale-[1.2]">
-          <WrdoLogo className="w-6 h-6" />
-        </a>
-        <a
-          href="mailto:hi@oiov.dev"
+          href="mailto:hi@httpsmail.com"
           title="Email"
           className="text-gray-400 hover:text-gray-500">
           <MailIcon className="w-6 h-6" />
-        </a>
-        <a
-          href="https://twitter.com/yesmoree"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Twitter"
-          className="text-gray-400 hover:text-gray-500">
-          <Twitter />
-        </a>
-        <a
-          href="https://github.com/oiov/vmail"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Github"
-          className="text-gray-400 hover:text-gray-500">
-          <GitHub />
         </a>
       </div>
     </div>
